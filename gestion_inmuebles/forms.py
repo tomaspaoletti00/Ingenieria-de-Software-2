30a6990393f2daf81aee8bb21f4bb4a6c60429d2
Lautaro <lauljfa05@gmail.com>
2025-05-09 17:55:49 -0300

    Formularios para todos los tipos de inmueble. No tuve encuenta permisos, para acceder tenes que ir a /inmuebles/formularioInmueble

--- gestion_inmuebles/forms.py
+++ gestion_inmuebles/forms.py
@@ -1,7 +1,30 @@
-from django import forms
-from .models import Inmueble
+from django.forms import ModelForm
+from .models import Inmueble, Departamento, Casa, Local, Cochera
 
-class InmuebleForm(forms.ModelForm):
+class InmuebleForm(ModelForm):
     class Meta:
         model = Inmueble
+        fields = '__all__'
+
+#   Formularios para Departamento, Casa, Local y Cochera
+#   Tecnicamente se puede borrar el de arriba pero por las dudas no toco nada
+
+class FormularioDepartamento(ModelForm):
+    class Meta:
+        model = Departamento
+        fields = '__all__'
+
+class FormularioCasa(ModelForm):
+    class Meta:
+        model = Casa
+        fields = '__all__'
+
+class FormularioLocal(ModelForm):
+    class Meta:
+        model = Local
+        fields = '__all__'
+
+class FormularioCochera(ModelForm):
+    class Meta:
+        model = Cochera
         fields = '__all__'
--- gestion_inmuebles/views.py
+++ gestion_inmuebles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from django.shortcuts import render, redirect
-from .forms import InmuebleForm
+from .forms import InmuebleForm, FormularioDepartamento, FormularioCasa, FormularioLocal, FormularioCochera
 from .models import Inmueble
 
 def adminInmuebles(request):
@@ -20,3 +20,77 @@ def crear_inmueble(request):
 def listarInmuebles(request):
     inmuebles = Inmueble.objects.all()  # Esto sería el equivalente a SELECT * FROM inmueble
     return render(request, 'gestion_inmuebles/listaInmuebles.html', {'inmuebles': inmuebles})
+
+# Esto es todo lo de la parte para agregar inmuebles de los cuatro tipos
+# Hay varios def con codigo muy parecido, si fuera oo2 seria como un homicidio triple
+# Despues le hago refactoring lo importante es que anda y se guarda en la base de datos
+
+def formulario_inmueble(request):
+    return render(request, 'gestion_inmuebles/formulario_inmueble.html')
+
+def crear_formulario(request):
+    formulario_tipo = {
+        'departamento': crear_departamento,
+        'casa': crear_casa,
+        'local': crear_local,
+        'cochera': crear_cochera
+    }
+
+    tipo = request.GET.get('tipo')
+
+    if tipo not in formulario_tipo.keys():
+        return redirect('gestion_inmuebles/formulario_inmueble.html')
+
+    return formulario_tipo[tipo](request)
+
+def crear_departamento(request):
+    form = FormularioDepartamento(request.POST or None, request.FILES or None)
+    context = {
+        'form': form,
+        'tipo': 'departamento'
+    }
+
+    if form.is_valid():
+        form.save()
+        redirect('gestion_inmuebles/formulario_inmueble.html')
+
+    return render(request, 'gestion_inmuebles/formulario_generico.html', context)
+
+def crear_casa(request):
+    form = FormularioCasa(request.POST or None, request.FILES or None)
+    context = {
+        'form': form,
+        'tipo': 'casa'
+    }
+
+    if form.is_valid():
+        form.save()
+        redirect('gestion_inmuebles/formulario_inmueble.html')
+
+    return render(request, 'gestion_inmuebles/formulario_generico.html', context)
+
+def crear_local(request):
+    form = FormularioLocal(request.POST or None, request.FILES or None)
+    context = {
+        'form': form,
+        'tipo': 'local'
+    }
+
+    if form.is_valid():
+        form.save()
+        redirect('gestion_inmuebles/formulario_inmueble.html')
+
+    return render(request, 'gestion_inmuebles/formulario_generico.html', context)
+
+def crear_cochera(request):
+    form = FormularioCochera(request.POST or None, request.FILES or None)
+    context = {
+        'form': form,
+        'tipo': 'cochera'
+    }
+
+    if form.is_valid():
+        form.save()
+        redirect('gestion_inmuebles/formulario_inmueble.html')
+
+    return render(request, 'gestion_inmuebles/formulario_generico.html', context)
