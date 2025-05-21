34e1a1ab04d586b3783649c3b1b78360f067629c
Valentino Ongaro <valentinoongaro@hotmail.com>
2025-05-21 15:56:23 -0300

    Pusheando media Demo

--- gestion_inmuebles/forms.py
+++ gestion_inmuebles/forms.py
@@ -1,30 +1,49 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, NumberInput
 from .models import Inmueble, Departamento, Casa, Local, Cochera
 
-class InmuebleForm(ModelForm):
-    class Meta:
-        model = Inmueble
-        fields = '__all__'
+class InmuebleForm(ModelForm):       
+        
+        class Meta:
+            model = Inmueble
+            fields = '__all__'
+
+        def clean(self):
+            cleaned_data = super().clean()
+
+            campos_a_validar = ['precio']  # Agregá los campos que quieras validar
+
+            for campo in campos_a_validar:
+                valor = cleaned_data.get(campo)
+                if valor is not None and valor <= 0:
+                    self.add_error(campo, f'El campo {campo} debe ser mayor a cero.')
+
+            return cleaned_data
+
 
 #   Formularios para Departamento, Casa, Local y Cochera
 #   Tecnicamente se puede borrar el de arriba pero por las dudas no toco nada
 
-class FormularioDepartamento(ModelForm):
-    class Meta:
-        model = Departamento
-        fields = '__all__'
 
-class FormularioCasa(ModelForm):
+class FormularioDepartamento(InmuebleForm):
     class Meta:
+        model = Departamento    
+        fields = '__all__'
+        
+
+class FormularioCasa(InmuebleForm):
+    class Meta(InmuebleForm):
         model = Casa
         fields = '__all__'
 
-class FormularioLocal(ModelForm):
-    class Meta:
+
+class FormularioLocal(InmuebleForm):    
+    class Meta(InmuebleForm):
         model = Local
         fields = '__all__'
 
-class FormularioCochera(ModelForm):
-    class Meta:
+class FormularioCochera(InmuebleForm):
+    class Meta(InmuebleForm):
         model = Cochera
         fields = '__all__'
+
+
--- gestion_inmuebles/urls.py
+++ gestion_inmuebles/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('crear/', views.crear_inmueble, name='crear_inmueble'),
     path('adminInmuebles/', views.adminInmuebles, name='adminInmuebles'),
-    path('listaInmuebles/', views.listarInmuebles, name='listaInmuebles'),
+    path('listaInmuebles/', views.listar_Inmuebles, name='listaInmuebles'),
     path('formularioInmueble/', views.formulario_inmueble, name='formularioInmueble'),
     path('formulario/', views.crear_formulario),
+    path('inmuebles/', views.listar_Inmuebles, name='listar_Inmuebles'),
+    path('inmuebles/<int:pk>/', views.inmueble_detalle, name='inmueble_detalle')
 ]
